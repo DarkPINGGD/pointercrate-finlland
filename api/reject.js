853b41549fca97b014d0a2a389adf840da3fe758
DARKPINGGD <omerasarfino@gmail.com>
2025-12-23 18:58:30 +0300

    Update reject.js

--- api/reject.js
+++ api/reject.js
@@ -1,14 +1,14 @@
 import fs from "fs";
 import path from "path";
 
-export default function(req,res){
-  const file = path.join(process.cwd(),"data/data.json");
+export default function handler(req, res) {
+  const file = path.join(process.cwd(), "data/data.json");
   const data = JSON.parse(fs.readFileSync(file));
 
-  if(req.body.password!==data.adminPassword)
+  if (req.body.password !== data.adminPassword)
     return res.status(401).end();
 
-  data.records.splice(req.body.index,1);
-  fs.writeFileSync(file,JSON.stringify(data,null,2));
+  data.records.splice(req.body.index, 1);
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
   res.end();
 }
