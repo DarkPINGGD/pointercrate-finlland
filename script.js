9d5e6db678a3cb84fa51415b304b4192d99167ad
DARKPINGGD <omerasarfino@gmail.com>
2025-12-23 19:00:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let last = 0;
 
 function submit(){
-  if(Date.now()-last<5000){ alert("Wait 5 seconds"); return; }
+  if(Date.now() - last < 5000){
+    alert("Wait 5 seconds");
+    return;
+  }
   last = Date.now();
 
   fetch("/api/submit",{
     method:"POST",
-    headers:{"Content-Type":"application/json"},
+    headers:{ "Content-Type":"application/json" },
     body:JSON.stringify({
       player:player.value,
       country:country.value,
       level:level.value,
       video:video.value
     })
-  }).then(r=>alert("Submitted for review"));
+  })
+  .then(r=>r.json())
+  .then(d=>alert(d.success?"Submitted":"Error"));
 }
