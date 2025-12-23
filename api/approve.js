afeb597a8d466bce2809f2ace4e9fd4f3d36f5bd
DARKPINGGD <omerasarfino@gmail.com>
2025-12-23 18:56:21 +0300

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,14 +1,14 @@
 import fs from "fs";
 import path from "path";
 
-export default function(req,res){
-  const file = path.join(process.cwd(),"data/data.json");
+export default function handler(req, res) {
+  const file = path.join(process.cwd(), "data/data.json");
   const data = JSON.parse(fs.readFileSync(file));
 
-  if(req.body.password!==data.adminPassword)
+  if (req.body.password !== data.adminPassword)
     return res.status(401).end();
 
   data.records[req.body.index].approved = true;
-  fs.writeFileSync(file,JSON.stringify(data,null,2));
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
   res.end();
 }
